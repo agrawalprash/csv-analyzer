ab424336d9ba6354f61be24c0608355a52ae5c43
Prashant Agrawal <prashant@enthought.com>
2014-10-28 18:27:27 +0530

    Add a controller which doesn't do any real work but lets us test the view logic

---
+++ csv-analyzer/client/scripts/app/controllers.js
@@ -0,0 +1,35 @@
+var app = angular.module('CSVApp');
+
+app.controller('FileUploadController', ['$scope', function($scope){
+
+    $scope.init = function() {
+        $scope.file_chosen = false;
+        $scope.upload_started = false;
+        $scope.upload_paused = false;
+    };
+
+    $scope.choose_file = function() {
+        $scope.file_chosen = true;
+    };
+
+    $scope.start_upload = function() {
+        $scope.upload_started = true;
+    };
+
+    $scope.pause_upload = function() {
+        $scope.upload_paused = true;
+    };
+
+    $scope.resume_upload = function() {
+        $scope.upload_started = true;
+        $scope.upload_paused = false;
+    };
+
+    $scope.cancel_upload = function() {
+        $scope.init();
+    };
+
+    // Initialize the scope.
+    $scope.init();
+
+}]);
