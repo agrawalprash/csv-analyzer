0a920e7d10664eb36764d81c94ca1e69262460ca
Prashant Agrawal <prashant@enthought.com>
2014-10-28 18:54:16 +0530

    Add a placeholder FileUploadService.

--- csv-analyzer/client/scripts/app/controllers.js
+++ csv-analyzer/client/scripts/app/controllers.js
@@ -3,8 +3,8 @@ var app = angular.module('CSVApp');
 app.controller(
     'FileUploadController',
 
-    ['$scope', 'UploadService',
-    function($scope, UploadService){
+    ['$scope', 'FileUploadService',
+    function($scope, FileUploadService){
 
         $scope.init = function() {
             $scope.file_chosen = false;
@@ -14,22 +14,34 @@ app.controller(
 
         $scope.choose_file = function() {
             $scope.file_chosen = true;
+
+            // putting dummy values for now. this would be some `File` object
+            // ultimately
+            $scope.file = {};
         };
 
         $scope.start_upload = function() {
+            FileUploadService.start_upload($scope.file);
+
             $scope.upload_started = true;
         };
 
         $scope.pause_upload = function() {
+            FileUploadService.pause_upload($scope.file);
+
             $scope.upload_paused = true;
         };
 
         $scope.resume_upload = function() {
+            FileUploadService.resume_upload($scope.file);
+
             $scope.upload_started = true;
             $scope.upload_paused = false;
         };
 
         $scope.cancel_upload = function() {
+            FileUploadService.cancel_upload($scope.file);
+
             $scope.init();
         };
 
---
+++ csv-analyzer/client/scripts/app/services.js
@@ -0,0 +1,28 @@
+var app = angular.module('CSVApp');
+
+app.factory(
+    'FileUploadService',
+
+    function(){
+
+        var FileUploadService = {};
+
+        FileUploadService.start_upload = function(file) {
+
+        };
+
+        FileUploadService.pause_upload = function(file) {
+
+        };
+
+        FileUploadService.resume_upload = function(file) {
+
+        };
+
+        FileUploadService.cancel_upload = function(file) {
+
+        };
+
+        return FileUploadService;
+    }
+);
